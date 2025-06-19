984b58be86fedcb12f53acedc07ab24404395074
Yangfan Zhou <140487713+Yangfan9606@users.noreply.github.com>
2025-06-19 14:04:30 +0800

    run.sh

---
+++ Hydrogel single-cell sequencing/run.sh
@@ -0,0 +1,13 @@
+# INPUT with read1.fastq.gz, read2.fastq.gz
+INPUT="input_sample_name"
+
+perl Hydrogel_seq_Read1_Barcode.check.pl $INPUT.read1.fastq.gz -b1 barcode1_TSO.txt -b2 barcode2.txt -b3 barcode3.txt -o $INPUT.R1
+perl trim.fastq.pl $INPUT.R1.BC_check.gz $INPUT.read1.fastq.gz $INPUT.read2.fastq.gz -o $INPUT
+perl fix.R1R2.clipper.pl $INPUT.1.fastq.clipper.gz $INPUT.2.fastq.clipper.gz -o $INPUT
+perl To.10x.fastq.pl 3M-february-2018.txt.gz $INPUT.2.fix.fastq.gz $INPUT.R1.BC_check.gz $INPUT.1.fix.fastq.gz -o $INPUT
+ln -s $INPUT.For10x.1.fastq.gz ${INPUT}_S1_L001_R1_001.fastq.gz
+ln -s $INPUT.For10x.2.fastq.gz ${INPUT}_S1_L001_R2_001.fastq.gz
+
+# Run with cellranger , download 10x
+INPUT_ID="sample_ID"
+cellranger count --id=$INPUT_ID --transcriptome=./refdata-gex-GRCh38-and-mm10-2020-A/ --fastqs=./ --sample=$INPUT --chemistry=SC3Pv3 --localcores 12
